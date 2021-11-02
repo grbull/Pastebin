c0b141ce9bd6d27578a769a45fea22cf69248c35
Graham Bull <graham.bull@gmail.com>
2021-11-02 11:37:25 +0000

    added validation attributes to SnippetCreateModel, also enabled nullable annotations

--- src/Pastebin.Web/Models/SnippetCreateModel.cs
+++ src/Pastebin.Web/Models/SnippetCreateModel.cs
@@ -1,15 +1,22 @@
+#nullable enable
+using System.ComponentModel.DataAnnotations;
+
 namespace Pastebin.Web.Models
 {
     public class SnippetCreateModel
     {
+        [StringLength(50)]
         public string? Title { get; set; }
 
+        [StringLength(10)]
         public string? Language { get; set; }
 
         public bool IsPrivate { get; set; }
 
+        [StringLength(5000)]
         public string Content { get; set; }
 
+        [Range(0, 1440)]
         public int? ExpiresInMin { get; set; }
     }
 }
