ae33aa328ebbf2e2146a75fc2702de5a23d5cb1a
Graham Bull <graham.bull@gmail.com>
2021-11-02 13:20:10 +0000

    add display name attributes to SnippetCreateModel

--- src/Pastebin.Web/Models/SnippetCreateModel.cs
+++ src/Pastebin.Web/Models/SnippetCreateModel.cs
@@ -11,11 +11,13 @@ namespace Pastebin.Web.Models
         [StringLength(10)]
         public string? Language { get; set; }
 
+        [Display(Name = "Private")]
         public bool IsPrivate { get; set; }
 
         [StringLength(5000)]
         public string Content { get; set; }
 
+        [Display(Name = "Expires in")]
         [Range(0, 1440)]
         public int? ExpiresInMin { get; set; }
     }
