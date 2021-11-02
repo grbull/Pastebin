cd12cef39c2e1381fc1dc6e3d2a4260edf5bd009
Graham Bull <graham.bull@gmail.com>
2021-11-02 13:19:54 +0000

    add display name attributes to SnippetModel

--- src/Pastebin.Web/Models/SnippetModel.cs
+++ src/Pastebin.Web/Models/SnippetModel.cs
@@ -14,12 +14,15 @@ namespace Pastebin.Web.Models
         [DisplayFormat(ConvertEmptyStringToNull = true, NullDisplayText = "None")]
         public string? Language { get; set; }
 
+        [Display(Name = "Private")]
         public bool IsPrivate { get; set; }
 
         public string Content { get; set; }
 
+        [Display(Name = "Date Created")]
         public DateTime DateCreated { get; set; }
 
+        [Display(Name = "Date Expires")]
         [DisplayFormat(NullDisplayText = "Never")]
         public DateTime? DateExpires { get; set; }
     }
