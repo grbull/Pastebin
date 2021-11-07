cde793157a718dc678b039a9f27db0e0dd37b595
Graham Bull <graham.bull@gmail.com>
2021-11-07 14:13:45 +0000

    added tests for SnippetRepository.GetRecent
    now resets database every test

--- test/Pastebin.Tests/Data/Repositories/SnippetRepositoryTests.cs
+++ test/Pastebin.Tests/Data/Repositories/SnippetRepositoryTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -23,6 +24,12 @@ namespace Pastebin.Tests.Data.Repositories
         public async Task AddAsync_ShouldAddSnippet_WhenSnippetIsValid()
         {
             // Arrange
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.EnsureCreatedAsync();
+            }
+
             var testDateCreated = DateTime.UtcNow;
             var testSnippet = new Snippet
             {
@@ -55,6 +62,12 @@ namespace Pastebin.Tests.Data.Repositories
         public async Task FindAsync_ShouldReturnSnippet_WhenSnippetExists()
         {
             // Arrange
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.EnsureCreatedAsync();
+            }
+
             var testDateCreated = DateTime.UtcNow;
             var testSnippet = new Snippet
             {
@@ -88,16 +101,184 @@ namespace Pastebin.Tests.Data.Repositories
         [Fact]
         public async Task FindAsync_ShouldReturnNull_WhenSnippetDoesNotExist()
         {
-            // Arrange & Act
+            // Arrange
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.EnsureCreatedAsync();
+            }
+
             var testId = Guid.NewGuid();
 
-            // Assert
+            // Act & Assert
             using (var context = new PastebinContext(_contextOptions))
             {
                 var repository = new SnippetRepository(context);
                 var snippet = await repository.FindAsync(testId);
 
                 snippet.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public async Task GetRecentAsync_ShouldReturnAListOfSnippets()
+        {
+            // Arrange
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.EnsureCreatedAsync();
+            }
+
+            var testDateCreated = DateTime.UtcNow;
+            var testSnippet = new Snippet
+            {
+                Id = Guid.NewGuid(),
+                Title = "Test Snippet",
+                Language = "c#",
+                IsPrivate = false,
+                Content = "Test Content",
+                DateCreated = testDateCreated,
+                DateExpires = testDateCreated.AddMinutes(60),
+            };
+
+            // Act
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                await context.AddAsync(testSnippet);
+                await context.SaveChangesAsync();
+            }
+
+            // Assert
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                var repository = new SnippetRepository(context);
+                var snippets = await repository.GetRecentAsync(1);
+
+                snippets.Should().BeOfType<List<Snippet>>()
+                    .And.ContainSingle().Which.Id.Should().Be(testSnippet.Id);
+            }
+        }
+
+        [Fact]
+        public async Task GetRecentAsync_ShouldNotReturnPrivateSnippets()
+        {
+            // Arrange
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.EnsureCreatedAsync();
+            }
+
+            var testSnippet = new Snippet
+            {
+                Id = Guid.NewGuid(),
+                IsPrivate = true,
+                Content = "Test Content",
+                DateCreated = DateTime.UtcNow,
+            };
+
+            // Act
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                await context.AddAsync(testSnippet);
+                await context.SaveChangesAsync();
+            }
+
+            // Assert
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                var repository = new SnippetRepository(context);
+                var snippets = await repository.GetRecentAsync(1);
+
+                snippets.Should().BeOfType<List<Snippet>>().And.BeEmpty();
+            }
+        }
+
+        [Fact]
+        public async Task GetRecentAsync_ShouldNotReturnExpiredSnippets()
+        {
+            // Arrange
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.EnsureCreatedAsync();
+            }
+
+            var testSnippet = new Snippet
+            {
+                Id = Guid.NewGuid(),
+                IsPrivate = false,
+                Content = "Test Content",
+                DateCreated = DateTime.MinValue,
+                DateExpires = DateTime.MinValue.AddMinutes(60)
+            };
+
+            // Act
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                await context.AddAsync(testSnippet);
+                await context.SaveChangesAsync();
+            }
+
+            // Assert
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                var repository = new SnippetRepository(context);
+                var snippets = await repository.GetRecentAsync(1);
+
+                snippets.Should().BeOfType<List<Snippet>>().And.BeEmpty();
+            }
+        }
+
+        [Fact]
+        public async Task GetRecentAsync_ShouldReturnSnippetsDescendingByDateCreated()
+        {
+            // Arrange
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                await context.Database.EnsureDeletedAsync();
+                await context.Database.EnsureCreatedAsync();
+            }
+
+            var testSnippets = new List<Snippet>
+            {
+                new()
+                {
+                    Id = Guid.NewGuid(), Content = "Oldest", IsPrivate = false, DateCreated = DateTime.UtcNow
+                },
+                new()
+                {
+                    Id = Guid.NewGuid(), Content = "Test Content", IsPrivate = false,
+                    DateCreated = DateTime.UtcNow.AddMinutes(30)
+                },
+                new()
+                {
+                    Id = Guid.NewGuid(), Content = "Newest", IsPrivate = false,
+                    DateCreated = DateTime.UtcNow.AddMinutes(60)
+                }
+            };
+            testSnippets.Sort((a, b) => DateTime.Compare(b.DateCreated, a.DateCreated));
+
+            // Act
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                foreach (var testSnippet in testSnippets)
+                {
+                    await context.AddAsync(testSnippet);
+                }
+
+                await context.SaveChangesAsync();
+            }
+
+            // Assert
+            using (var context = new PastebinContext(_contextOptions))
+            {
+                var repository = new SnippetRepository(context);
+                var snippets = await repository.GetRecentAsync(3);
+
+                snippets.Should().NotBeEmpty().And.HaveCount(3).And
+                    .BeEquivalentTo(testSnippets, options => options.WithStrictOrdering());
             }
         }
     }
